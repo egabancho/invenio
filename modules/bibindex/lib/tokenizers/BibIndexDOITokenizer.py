3f4570b9b08847599433f237caa9bf96530dacf0
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2014-07-11 16:35:36 +0200

    BibIndex: missing import on `BibIndexDOITokenizer`
    Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

--- modules/bibindex/lib/tokenizers/BibIndexDOITokenizer.py
+++ modules/bibindex/lib/tokenizers/BibIndexDOITokenizer.py
@@ -17,6 +17,7 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 from invenio.bibindex_tokenizers.BibIndexFilteringTokenizer import BibIndexFilteringTokenizer
+from invenio.bibfield import get_record
 
 
 class BibIndexDOITokenizer(BibIndexFilteringTokenizer):
