ad1e0bcd571bf28fc90347365db6407bb737b9ed
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2014-10-15 16:49:43 +0200

    records: better PID list
    Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

--- invenio/modules/records/api.py
+++ invenio/modules/records/api.py
@@ -141,7 +141,9 @@ class Record(SmartJson):
                         tmp.extend(self._massage_pid_value((tuple_[0], item)))
                     return tmp
                 else:
-                    return [{tuple_[0]:tuple_[1], 'type':'', 'provider':''}]
+                    return [{'value': tuple_[1],
+                             'type': tuple_[0],
+                             'provider':'invenio'}]
 
         pids = PIDList()
         for key, value in self.items(without_meta_metadata=True):
