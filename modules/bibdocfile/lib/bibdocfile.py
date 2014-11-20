egabancho/invenio

commit 4acc073172179038db21ec561e7f4fdf57fceec5

BibDocFile: bibdocfile.BibDoc memory fix
* Fixes a memory leak in BibDocFile module where BibDoc instances were
  referring to BibDocFiles and viceversa, by using weakref.ref().
  (closes #2082) (PR #2136)

Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>
Reported-by: Thorsten Schwander <thorsten.schwander@gmail.com>
Tested-by: Tibor Simko <tibor.simko@cern.ch>

modules/bibdocfile/lib/bibdocfile.py
@@ -79,6 +79,8 @@ except ImportError:
 from datetime import datetime
 from mimetypes import MimeTypes
 from thread import get_ident
+from weakref import ref
+
 
 from invenio import webinterface_handler_config as apache
 
@@ -2924,7 +2926,9 @@ class BibDocFile(object):
     """This class represents a physical file in the Invenio filesystem.
     It should never be instantiated directly"""
 
-    def __init__(self, fullpath, recid_doctypes, version, docformat, docid, status, checksum, more_info=None, human_readable=False, cd=None, md=None, size=None, bibdoc = None):
+    def __init__(self, fullpath, recid_doctypes, version, docformat, docid,
+                 status, checksum, more_info=None, human_readable=False,
+                 cd=None, md=None, size=None, bibdoc=None):
         self.fullpath = os.path.abspath(fullpath)
 
         self.docid = docid
@@ -2936,7 +2940,10 @@ class BibDocFile(object):
         self.checksum = checksum
         self.human_readable = human_readable
         self.name = recid_doctypes[0][2]
-        self.bibdoc = bibdoc
+        if bibdoc is not None:
+            self.__bibdoc = ref(bibdoc)
+        else:
+            self.__bibdoc = None
 
         if more_info:
             self.description = more_info.get_description(docformat, version)
@@ -2974,6 +2981,17 @@ class BibDocFile(object):
         self.etag = '"%i%s%i"' % (self.docid, self.format, self.version)
         self.magic = None
 
+    @property
+    def bibdoc(self):
+        """
+        Wrapper around the referenced bibdoc necesseary to avoid memory leaks.
+        """
+        if self.__bibdoc is None or self.__bibdoc() is None:
+            bibdoc = BibDoc(self.docid)
+            self.__bibdoc = ref(bibdoc)
+            return bibdoc
+        return self.__bibdoc()
+
     def __repr__(self):
         return ('BibDocFile(%s,  %i, %s, %s, %i, %i, %s, %s, %s, %s)' % (repr(self.fullpath), self.version, repr(self.name), repr(self.format), self.recids_doctypes[0][0], self.docid, repr(self.status), repr(self.checksum), repr(self.more_info), repr(self.human_readable)))
 
@@ -2981,6 +2999,7 @@ class BibDocFile(object):
         if self.bibdoc:
             return self.bibdoc.format_recids()
         return "0"
+
     def __str__(self):
         recids = self.format_recids()
         out = '%s:%s:%s:%s:fullpath=%s\n' % (recids, self.docid, self.version, self.format, self.fullpath)

modules/bibdocfile/lib/bibdocfile_regression_tests.py
@@ -27,7 +27,7 @@ from invenio.testutils import InvenioTestCase
 from invenio.testutils import make_test_suite, run_test_suite
 from invenio.bibdocfile import BibRecDocs, BibRelation, MoreInfo, \
     check_bibdoc_authorization, bibdocfile_url_p, guess_format_from_url, CFG_HAS_MAGIC, \
-    Md5Folder, calculate_md5, calculate_md5_external
+    Md5Folder, calculate_md5, calculate_md5_external, BibDoc
 from invenio.dbquery import run_sql
 
 from invenio.access_control_config import CFG_WEBACCESS_WARNING_MSGS
@@ -597,6 +597,24 @@ class BibDocFileMd5FolderTests(InvenioTestCase):
             open(filepath, "w").write("test")
             self.assertEqual(calculate_md5(filepath, force_internal=True), calculate_md5_external(filepath))
 
+
+class BibDocFileMemoryLeak(InvenioTestCase):
+    def test_for_memory_leaks(self):
+        """bibdocfile - test for memory leaks"""
+        import gc
+
+        def foo(docid):
+            bd = BibDoc(docid)
+            dummy_bf = bd.list_all_files()
+
+        gc.collect()
+        self.assertEqual(gc.garbage, [])
+        for docid in range(1,11):
+            foo(docid)
+        gc.collect()
+        self.assertEqual(gc.garbage, [])
+
+
 TEST_SUITE = make_test_suite(BibDocFileMd5FolderTests,
                              BibRecDocsTest,
                              BibDocsTest,
@@ -606,6 +624,7 @@ TEST_SUITE = make_test_suite(BibDocFileMd5FolderTests,
                              BibDocFileURLTest,
                              CheckBibDocAuthorizationTest,
                              BibDocFsInfoTest,
-                             BibDocFileGuessFormat)
+                             BibDocFileGuessFormat,
+                             BibDocFileMemoryLeak)
 if __name__ == "__main__":
     run_test_suite(TEST_SUITE, warn_user=True)

commit ca61f0cf4895b086bd58e2823200272e16d10d92

BibDocFile: pickle support fix
* Fixes errors when trying to pickle BibDocFile objects due to the
  weakref introduced in 4acc073172179038db21ec561e7f4fdf57fceec5.

Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

modules/bibdocfile/lib/bibdocfile.py
@@ -58,6 +58,7 @@ import base64
 import binascii
 import cgi
 import sys
+import copy
 
 if sys.hexversion < 0x2060000:
     from md5 import md5
@@ -2991,6 +2992,23 @@ class BibDocFile(object):
             self.__bibdoc = ref(bibdoc)
             return bibdoc
         return self.__bibdoc()
+
+    def __getstate__(self):
+        """Remove weakref so the object can be pickle."""
+        dict_ = copy.copy(self.__dict__)
+        dict_['_BibDocFile__bibdoc'] = self.bibdoc
+        return  dict_
+
+    def __setstate__(self, data_dict):
+        """Undo what `__getstate__` did setting back the weakref.
+
+        :param data_dict: `dict` from `__getstate__`
+        """
+        for (name, value) in data_dict.iteritems():
+            setattr(self, name, value)
+
+        if self.__bibdoc is not None:
+            self.__bibdoc = ref(self.__bibdoc)
 
     def __repr__(self):
         return ('BibDocFile(%s,  %i, %s, %s, %i, %i, %s, %s, %s, %s)' % (repr(self.fullpath), self.version, repr(self.name), repr(self.format), self.recids_doctypes[0][0], self.docid, repr(self.status), repr(self.checksum), repr(self.more_info), repr(self.human_readable)))